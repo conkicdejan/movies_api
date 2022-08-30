d7f839665acd4dc7039f97f5b245b5b0220c0040
Dejan Conkic <conkicdejan@gmail.com>
2022-08-30 12:12:14 +0200

    Add register & login pages

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth',
+    'controller' => AuthController::class
+], function ($router) {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+    Route::post('logout', 'logout')->middleware('auth');
+    Route::get('me', 'me')->middleware('auth');
 });
