4b12a508ba16ffb310b35972c395fcab45a6d8a0
Dejan Conkic <conkicdejan@gmail.com>
2022-09-13 11:26:52 +0200

    Test retrying failed jobs

--- app/Jobs/SendMailJob.php
+++ app/Jobs/SendMailJob.php
@@ -12,10 +12,16 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
+use Throwable;
 
 class SendMailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    // Test retrying of a single failed job
+    public $tries = 3;
+    public $backoff = 3;
+
 
     /**
      * Create a new job instance.
@@ -34,6 +40,17 @@ class SendMailJob implements ShouldQueue
      */
     public function handle()
     {
-        Mail::send(new MovieCreated($this->event->movie));
+        // Mail::send(new MovieCreated($this->event->movie));
+        throw new \Exception('Job failed');
     }
+
+    public function failed(Throwable $exception)
+    {
+    }
+
+    // Test retrying all of the failed jobs
+    // php artisan queue:retry all
+
+    // Test permanently deleting failed jobs
+    // php artisan queue:flush
 }
